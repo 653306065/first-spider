58844fe8d40f0193e795216768a91d54a84fb0a6
fuqiang <653306065@qq.com>
2020-12-07 12:38:40 +0800

    Update Spider.java

--- src/main/java/org/example/Spider.java
+++ src/main/java/org/example/Spider.java
@@ -5,6 +5,7 @@ import org.example.tools.OKHttpUtils;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
+
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.OutputStream;
@@ -32,34 +33,17 @@ public class Spider {
             //获取指定页的html结果
             Document document = JsoupUtil.getDocument(topUrl + startPage);
             //没有获取到html就跳过这次
-            if(Objects.isNull(document)){
+            if (Objects.isNull(document)) {
                 continue;
             }
             //获取获取预览图列表，获取方法有点类型 js里面取html上的元素
             Elements elements = document.getElementsByClass("thumb-listing-page").get(0).getElementsByTag("ul").get(0).getElementsByTag("li");
-            for (Element element : elements) {
-                //获取预览图的id
-                String id = element.getElementsByTag("figure").get(0).attr("data-wallpaper-id");
-                //根据网页的结构，构造好对应的图片url
-                String context = id.substring(0, 2);
-                //png格式的url (png和jpg只会存在一个)
-                String pngImageUrl = "https://w.wallhaven.cc/full/" + context + "/wallhaven-" + id + ".png";
-                //jpg格式的url
-                String jpgImageUrl = "https://w.wallhaven.cc/full/" + context + "/wallhaven-" + id + ".jpg";
-                //获取png格式的图片
-                byte[] pngByte = OKHttpUtils.getBytes(pngImageUrl);
-                if (Objects.nonNull(pngByte)) {
-                    //保存图片
-                    byteToFile(pngByte, savePath + id + ".png");
-                }
-                //获取png格式的图片
-                byte[] jpgByte = OKHttpUtils.getBytes(jpgImageUrl);
-                if (Objects.nonNull(jpgByte)) {
-                    //保存图片
-                    byteToFile(jpgByte, savePath + id + ".jpg");
-                }
-                System.out.println(id + ",下载完成");
-            }
+
+//            for (Element element : elements) {
+//                saveImage(element);
+//            }
+
+            elements.stream().parallel().forEach(this::saveImage);
             //开始页等于结束页就退出
             if (startPage == endPage) {
                 break;
@@ -68,8 +52,34 @@ public class Spider {
         }
     }
 
+
+    private void saveImage(Element element) {
+        //获取预览图的id
+        String id = element.getElementsByTag("figure").get(0).attr("data-wallpaper-id");
+        //根据网页的结构，构造好对应的图片url
+        String context = id.substring(0, 2);
+        //png格式的url (png和jpg只会存在一个)
+        String pngImageUrl = "https://w.wallhaven.cc/full/" + context + "/wallhaven-" + id + ".png";
+        //jpg格式的url
+        String jpgImageUrl = "https://w.wallhaven.cc/full/" + context + "/wallhaven-" + id + ".jpg";
+        //获取png格式的图片
+        byte[] pngByte = OKHttpUtils.getBytes(pngImageUrl);
+        if (Objects.nonNull(pngByte)) {
+            //保存图片
+            byteToFile(pngByte, savePath + id + ".png");
+        }
+        //获取png格式的图片
+        byte[] jpgByte = OKHttpUtils.getBytes(jpgImageUrl);
+        if (Objects.nonNull(jpgByte)) {
+            //保存图片
+            byteToFile(jpgByte, savePath + id + ".jpg");
+        }
+        System.out.println(id + ",下载完成");
+    }
+
     /**
      * 把byte[]输出到指定文件
+     *
      * @param bytes
      * @param path
      */
